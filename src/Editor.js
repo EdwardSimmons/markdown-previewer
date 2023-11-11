d13875ad91c4b9743d15947ea8c3b8f637acd061
Ed Simmons <e.d.simmons@outlook.com>
2023-11-11 11:02:31 +0000

    WiP: styling

--- src/Editor.js
+++ src/Editor.js
@@ -6,7 +6,12 @@ class Editor extends Component {
     render() {
         return (
             <div className="editor-container">
-                <textarea id="editor" onChange={e => this.props.onChange(e.target.value)}>
+                <textarea
+                    id="editor"
+                    onChange={e => this.props.onChange(e.target.value)}
+                    rows={4}
+                    className="editor-textarea"
+                >
                     {this.props.content}
                 </textarea>
             </div>
